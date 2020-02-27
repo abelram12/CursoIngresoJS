3fd57f218faf05b4af85b2da9c3ab79f3d6a7a5f
abelram12 <60698424+abelram12@users.noreply.github.com>
2020-02-27 12:06:54 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
@@ -1,4 +1,79 @@
 function mostrar()
 {
+    var numero;
+    var letra;
+    var cantpar = 0;
+    var cantimpar = 0;
+    var cantcero = 0;
+    var promedioPos;
+    var contpos =0;
+    var acumuladorpost=0;
+    var sumaNegativos;
+    var numMax;
+    var letMax;
+    var numMin;
+    var letMin;
+    var seguir;
+    var flag=0;
+
+    do{
+
+        numero = parseInt(prompt("Ingrese un numero entre -100 y 100: "));
+            while( !( numero >= -100 && numero <= 100)){
+            numero = parseInt(prompt("Ingrese un numero entre -100 y 100: "));
+
+             }
+        letra = prompt("Ingrese una letra: ");
+        
+        if(numero%2 == 0){
+            
+            cantpar++;
+        
+        }else{
+
+            cantimpar++;
+        }
+
+        if(numero>0){
+            contpos ++;
+            acumuladorpost = acumuladorpost + numero;
+
+        }else if (numero<0) {
+            sumaNegativos = sumaNegativos + numero;
+
+        }else{
+            cantcero++;
+        }
+
+        if( numMax < numero || flag == 0 ){
+            numMax = numero;
+            letMax = letra;
+           // flag = 1 aca no se pone porque sino podria saltear el codigo de abajo
+        }
+
+        if(numMin > numero || flag == 0){
+            numMin = numero;
+            letMin = letra;
+            flag = 1;
+        }
+
+     seguir = prompt("quiere continuar? s/n");
+
+
+    }while( seguir == 's');
+
+    if( contpos > 0 ){
+         promedioPos = acumuladorpost/contpos;
+
+    } 
+
+    document.write("La cantidad de numeros pares es " + cantpar + "<br>" );
+    document.write("La cantidad de numeros impares es " + cantimpar + "<br>" );
+    document.write("La cantidad de numeros ceros es " + cantcero + "<br>" );
+    document.write("El promedio de los numeros positivos es " + promedioPos + "<br>" );
+    document.write("la suma de los numeros negativos es " + sumaNegativos + "<br>" );
+    document.write("El numero minimo es  " + numMin + " su letra es  " + letMin + "<br>" );
+    document.write("El numero maximo es  " + numMax + " su letra es  " + letMax + "<br>" );
+
 
 }
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
@@ -1,4 +1,53 @@
 function mostrar()
 {
+    var notas;
+    var notaBaja;
+    var sexo;
+    var sexoBajo;
+    var contadordenotas = 0;
+    var promedio = 0;
+    var cantvarones6 = 0;
+    var flag = 0;
+
+    for(var cantAlumnos = 0; cantAlumnos < 5; cantAlumnos++ ){
+
+        notas = parseInt(prompt("Ingrese la nota del alumno:"));
+        while(notas < 0 || notas > 10 || isNaN(notas)){
+
+            alert ("No es una nota valida");
+            notas = parseInt(prompt("Reingrese la nota del alumno:"));
+
+        }
+
+        sexo = prompt("ingrese el sexo del alumno:").toLowerCase();
+
+        while(sexo != "f" &&  sexo != "m"){
+
+            sexo = prompt("Reingrese la sexo del alumno:");
+
+        }
+
+        contadordenotas = contadordenotas + notas;
+
+        if( notaBaja > notas || flag == 0 ){
+            notaBaja = notas;
+            sexoBajo = sexo;
+            flag = 1;
+        }
+
+        if (notas >= 6 && sexo == "m"){
+
+            cantvarones6 ++;
+
+        }
+
+
+    }
+
+    promedio = contadordenotas / 5;
+
+    alert(" El promedio de las notas totales es: " + promedio + "\n La nota mas baja es: " + notaBaja + "\n El sexo de la nota mas baja es: " + sexoBajo + "\n la cantidad de varones con nota >=6: " + cantvarones6);
+
+
 
 }
